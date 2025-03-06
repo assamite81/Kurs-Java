11a2a8c8a581b53622943cb8be37c183365ae580
assamite81 <assamite81@gmail.com>
2025-03-06 08:14:08 +0100

    operatory porównania

---
+++ Edukacja/src/OperatoryPorownania.java
@@ -0,0 +1,20 @@
+// porownuja elementy rownania i zwracaja logiczna wartosc (true/false)
+public class OperatoryPorownania {
+
+public static void main(String[] args) {
+    int firstNumber = 4;
+    int secondNumber = 6;
+
+    boolean result = firstNumber > secondNumber;
+
+    System.out.println(result);
+    System.out.println(firstNumber == secondNumber);
+    System.out.println(firstNumber != secondNumber);
+    System.out.println(firstNumber > secondNumber);
+    System.out.println(firstNumber < secondNumber);
+    System.out.println(firstNumber >= secondNumber);
+    System.out.println(firstNumber <= secondNumber);
+    
+}
+
+}
