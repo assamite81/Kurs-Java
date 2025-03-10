086de85a953fe093de8e157ffd6e751226110643
assamite81 <assamite81@gmail.com>
2025-03-10 18:04:58 +0100

    instrukcja warunkowa Else If

---
+++ Edukacja/src/InstrukcjaWarunkowaElseIf.java
@@ -0,0 +1,23 @@
+public class InstrukcjaWarunkowaElseIf {
+
+    public static void main(String[] args) {
+        
+        int number = 12;
+
+        if (number == 0) {
+            System.out.println("Liczba równa zero"); 
+        }else if(number > 10) {
+            System.out.println("Numer większy od 10");
+        }
+         else if(number > 0) {
+            System.out.println("Liczba dodatnia");
+        } 
+        else if(number < -5) {
+            System.out.println("Liczba mniejsza od -5");
+        } else {
+            System.out.println("Zaden warunek nie został spełniony");
+        }
+    }
+}
+
+// kod jest wykonywany do momentu spelnienia warunku, nie probuje spelniac kolejnych warunkow
