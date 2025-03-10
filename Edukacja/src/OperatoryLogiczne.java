c1b73c2f6527d285cb2442a73f2db521b6cee244
assamite81 <assamite81@gmail.com>
2025-03-10 17:25:24 +0100

    operatory logiczne

---
+++ Edukacja/src/OperatoryLogiczne.java
@@ -0,0 +1,24 @@
+public class OperatoryLogiczne {
+
+    public static void main(String[] args) {
+        boolean firstValue = true;
+        boolean secondValue = false;
+        boolean thirdValue = false;
+        boolean fourthValue = true;
+
+        // && -> true wtedy gdy wyrazenia skladowe sa rowne true
+        System.out.println(firstValue && secondValue);  // false
+        System.out.println(firstValue && fourthValue);  // true
+        System.out.println(secondValue || thirdValue);  // false
+
+        // || lub -> true gdy jedno wyrazenie jest rowne true
+        System.out.println(firstValue || secondValue);  // true
+        System.out.println(firstValue || fourthValue);  // true
+        System.out.println(secondValue || thirdValue);  // false
+
+        // ! negacja - zwraca wartosc przeciwna do wyrazenia przed ktorym sie znajduje
+        System.out.println(!firstValue);    // false
+        System.out.println(!thirdValue);    // true
+        System.out.println(!(firstValue && secondValue));   // true
+    }
+}
